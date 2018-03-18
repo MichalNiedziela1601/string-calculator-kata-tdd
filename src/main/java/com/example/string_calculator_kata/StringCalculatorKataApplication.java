dc1265169a420f50fd6a62141949b9019b2c64fc
sunday <sunday1601@gmail.com>
2018-03-18 10:37:18 +0000

    Init commit

---
+++ src/main/java/com/example/string_calculator_kata/StringCalculatorKataApplication.java
@@ -0,0 +1,12 @@
+package com.example.string_calculator_kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringCalculatorKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringCalculatorKataApplication.class, args);
+	}
+}
