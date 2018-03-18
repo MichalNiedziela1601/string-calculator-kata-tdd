2e8f02fb7329fb20d737949dd07379176fed1c91
sunday <sunday1601@gmail.com>
2018-03-18 13:02:21 +0000

    Given two numbers with comma delimiter then return sum - pass

--- src/main/java/com/example/string_calculator_kata/StringCalculator.java
+++ src/main/java/com/example/string_calculator_kata/StringCalculator.java
@@ -3,7 +3,15 @@ package com.example.string_calculator_kata;
 public class StringCalculator {
 
     public int add(String input) {
+        int sum = 0;
         if(input.isEmpty()) return 0;
-        return Integer.valueOf(input);
+        if(input.contains(",")) {
+            String[] numbers = input.split(",");
+            sum = Integer.valueOf(numbers[0]) + Integer.valueOf(numbers[1]);
+        } else {
+            sum = Integer.valueOf(input);
+        }
+
+        return sum;
     }
 }
